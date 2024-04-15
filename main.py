d1458bfb8f58975b42bae4ac1dc57152bef4bab1
Cisterakas <jcister_220000001482@uic.edu.ph>
2024-04-15 10:25:43 +0800

    import fast api

--- main.py
+++ main.py
@@ -19,6 +19,14 @@ from model.add_admin_user import AddAdminUserRouter
 from model.payment import PaymentsRouter
 from fastapi.middleware.cors import CORSMiddleware
 
+from time import time
+from fastapi import FastAPI, __version__
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 
 
 
@@ -51,3 +59,33 @@ app.include_router(ClaimingInformationRouter, prefix="/api")
 app.include_router(CourierInformationRouter, prefix="/api")
 app.include_router(UserTransactionHistoryRouter, prefix="/api")
 app.include_router(UserFeedbackRouter, prefix="/api")
+
+
+
+html = f"""
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>FastAPI on Vercel</title>
+        <link rel="icon" href="/static/favicon.ico" type="image/x-icon" />
+    </head>
+    <body>
+        <div class="bg-gray-200 p-4 rounded-lg shadow-lg">
+            <h1>Hello from FastAPI@{__version__}</h1>
+            <ul>
+                <li><a href="/docs">/docs</a></li>
+                <li><a href="/redoc">/redoc</a></li>
+            </ul>
+            <p>Powered by <a href="https://vercel.com" target="_blank">Vercel</a></p>
+        </div>
+    </body>
+</html>
+"""
+
+@app.get("/")
+async def root():
+    return HTMLResponse(html)
+
+@app.get('/ping')
+async def hello():
+    return {'res': 'pong', 'version': __version__, "time": time()}
